bf144eff1de53d8ecdfda4546c14cec8fb577094
skranz <sebastian.kranz@uni-ulm.de>
2019-12-04 20:59:10 +0100

    Update travis tests

--- testex/create_et.r
+++ testex/create_et.r
@@ -6,6 +6,10 @@ rfiles = rfiles[!endsWith(rfiles,"speed_test.R")]
 rmd.files = list.files("vignettes",glob2rx("*.Rmd"),full.names = TRUE )
 #rfiles = rfiles[1]
 sources = testex_sources(ex.in.fun.files = rfiles, rmd.files=rmd.files)
+
+
 library(RelationalContracts)
+{
 et = testex_create(sources)
 saveRDS(et, "testex/et.Rds")
+}
