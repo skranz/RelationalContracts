2aacbc5ddc610bc62c35b36e13b8be7f98eaa770
skranz <sebastian.kranz@uni-ulm.de>
2019-12-04 22:02:17 +0100

    update run_et

--- testex/run_et.r
+++ testex/run_et.r
@@ -6,7 +6,7 @@ et = readRDS("testex/et.Rds")
 library(RelationalContracts)
 exemptions=testex_exemptions(funs = c("animate_capped_rne_history"),classes = c("relgame","visNetwork","gg","ggplot"))
 
-res = testex_run(et,log.file = "testex/log.Rmd",stat.file = "testex/stats.csv", exemptions=exemptions)
+res = testex_run(et,log.file = "testex/log.Rmd",stat.file = "testex/stats.csv", exemptions=exemptions,print.code = TRUE, print.output = TRUE)
 
 if (res$num.issues>0) {
   stop("Example tests failed!")
