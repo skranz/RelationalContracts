89c3d08fefae4cf118504d0200d56cf6f5dc7104
skranz <sebastian.kranz@uni-ulm.de>
2019-12-04 08:55:15 +0100

    updates

--- testex/create_et.r
+++ testex/create_et.r
@@ -3,8 +3,9 @@ library(testex)
 rfiles = list.files("inst/examples",glob2rx("*.R"),full.names = TRUE)
 rfiles = rfiles[!endsWith(rfiles,"speed_test.R")]
 
+rmd.files = list.files("vignettes",glob2rx("*.Rmd"),full.names = TRUE )
 #rfiles = rfiles[1]
-sources = testex_sources(ex.in.fun.files = rfiles)
+sources = testex_sources(ex.in.fun.files = rfiles, rmd.files=rmd.files)
 library(RelationalContracts)
 et = testex_create(sources)
 saveRDS(et, "testex/et.Rds")
--- testex/examples.r
+++
@@ -1,1 +0,0 @@
-x = mysquare(5)
